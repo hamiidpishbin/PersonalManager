00b36b1ea2b6b6a643a741381fd6745a6813c933
Hamid Pishbin <61520042+hamiidpishbin@users.noreply.github.com>
2025-04-12 23:51:01 +0330

    Add Some KeyCloak Configs - Not Working Yet

--- src/Services/Identity/KeyCloak.Auth.API/Program.cs
+++ src/Services/Identity/KeyCloak.Auth.API/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(o =>
+	{
+		o.RequireHttpsMetadata = false;
+		o.Audience = builder.Configuration["Authentication:Audience"];
+		o.MetadataAddress = builder.Configuration["Authentication:MetadataAddress"]!;
+		o.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidIssuer = builder.Configuration["Authentication:ValidIssuer"],
+		};
+	});
 
 var app = builder.Build();
 
