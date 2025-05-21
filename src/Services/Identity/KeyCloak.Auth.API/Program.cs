c38ee10d10d812fac27ed2ec6cf1921fa4877a07
Hamid Pishbin <61520042+hamiidpishbin@users.noreply.github.com>
2025-05-21 18:47:04 +0330

    Before Implementing Users

--- src/Services/Identity/KeyCloak.Auth.API/Program.cs
+++ src/Services/Identity/KeyCloak.Auth.API/Program.cs
@@ -20,5 +20,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
